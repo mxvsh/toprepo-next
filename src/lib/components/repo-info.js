129f261e3e54cbb16b316391317e9e1fac28af3a
xencodes <xencodes@icloud.com>
2022-07-09 08:18:41 +0530

    repo info

--- src/lib/components/repo-info.js
+++ src/lib/components/repo-info.js
@@ -1,8 +1,25 @@
+import dynamic from 'next/dynamic';
 import React from 'react';
-import { useGetRepoMutation } from 'lib/app/services/repo';
+import { Spinner, Center } from '@chakra-ui/react';
+import { useGetRepoReadmeQuery } from 'lib/app/services/repo';
 
-const RepoInfo = () => {
-	const [getRepo, response] = useGetRepoMutation();
+const RepoInfo = ({ repo }) => {
+	const { data, isLoading } = useGetRepoReadmeQuery(
+		{
+			owner: repo?.owner.login,
+			name: repo?.name,
+			default_branch: repo?.default_branch,
+		},
+		{ skip: !repo }
+	);
+
+	if (isLoading) {
+		return (
+			<Center>
+				<Spinner />
+			</Center>
+		);
+	}
 	return <div></div>;
 };
 
